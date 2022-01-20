612ca2b8af00820cd5969e4cc9a391957744efaf
Daniel Meyer <uqdwq@student.kit.edu>
2022-01-20 17:59:48 +0100

    completed basic main.rs skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::ops::Range;
 use std::{env, fs};
 use std::time::Instant;
 fn main() {
@@ -5,36 +7,73 @@ fn main() {
     println!("{:?}", args);
     let mode = &args[1];
     let filename = &args[2];
-    let text = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
     match mode.as_str() {
-        "topk" => top_k(&text),
-        "repeat" => repeat(&text),
-        "echo" => print!("{}",text),
+        "topk" => top_k(&content, &filename),
+        "repeat" => repeat(&content, &filename),
+        "echo" => print!("{}",content),
         _ => println!("{} isn't a valid parameter, please use echo, topk or repeat", mode)
     }
 }
 
-fn top_k(text: &str) {
+fn top_k(content: &str, filename: &str) {
+
+    // first step is to parse the file, i wont include this step into the time measurement
+    let start_construction = Instant::now();
     let mut x: usize = 0;
-    let now = Instant::now();
-    let queries: Vec<Vec<u32>> = Vec::new();
-
-    for (i, c) in text.chars().enumerate() {
-        if c == '\n' {
-            x = i;
-            break;
-        }
-    }
-    let n = text[0..x].parse::<usize>().unwrap();
-    println!("{}", n);
-    // omega expensive TODO
     
+    let mut queries: Vec<(u32,u32)> = Vec::new();
+    let mut text_begin: usize = 0;
 
-    // println!("k: {}", n);
+    // i also store the highest k for each patternlength to avoid multiple queues for the same length 
+    // useful in for example: example_text_topk_2.txt
+    let mut max_query: HashMap<u32, u32> = HashMap::new();
+    
+    parse_content_top_k(&mut queries, &mut max_query, &content, &mut text_begin);
 
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed)
+    // strings in rust are stored in unicode and know both their length in unicodechars and bytes. so this is cheap 
+    let text = content[text_begin..].as_bytes();
+
+
+    // 2nd step is to build the textindex i will be using SA (build with SAIS) and LCP-array 
+
+    // strings in rust arent NUL-terminated so we need to add a NULBYTE as sentinel later
+    let mut sa: Vec<i32> = vec![-1; text.len() + 1];
+    let mut lcp: Vec<i32> = vec![0; text.len() + 1];
+    build_sa(&text, &mut sa);
+    build_lcp(&text, &sa, &mut lcp);
+
+    let duration_construction = start_construction.elapsed();
+    // 3rd step queries
+    let start_q = Instant::now();
+    let mut result: String = top_k_query(&queries, &max_query, &sa, &lcp);
+    let duration_q = start_q.elapsed();
+
+    println!("RESULT algo=topk name=danielmeyer construction_time={:?} query_time={:?} solutions={} file={}", duration_construction.as_millis(), duration_q.as_millis(),result, filename)
+}
+
+fn parse_content_top_k(queries: &mut Vec<(u32, u32)>, max_query: &mut HashMap<u32, u32>, content: &str, text: &mut usize)  {}
+fn build_sa(text: &[u8], sa: &mut [i32]) {}
+fn build_lcp(text: &[u8], sa: &Vec<i32>, lcp: &Vec<i32>) {}
+fn top_k_query(queries: &Vec<(u32, u32)>, max_query: &HashMap<u32, u32>, sa: &Vec<i32>, lcp: &Vec<i32>) -> String {
+    String::from("Penis;Dick")
+}
+
+fn repeat(content: &str, filename: &str) {
+    let start_construction = Instant::now();
+    let text = content.as_bytes();
+    let mut sa: Vec<i32> = vec![-1; text.len() + 1];
+    let mut lcp: Vec<i32> = vec![0; text.len() + 1];
+    build_sa(&text, &mut sa);
+    build_lcp(&text, &sa, &mut lcp);
+    let duration_construction = start_construction.elapsed();
+    let start_q = Instant::now();
+    let result = longest_tandem_repeat(&text, &sa, &lcp);
+    let duration_q = start_q.elapsed();
+    println!("RESULT algo=repeat name=danielmeyer construction_time={} query_time={} solutions={} file={}", duration_construction.as_millis(), duration_q.as_millis(),result, filename)
 
 }
 
-fn repeat(text: &str) {}
+fn longest_tandem_repeat(text: &[u8], sa: &Vec<i32>, lcp: &Vec<i32>) -> String {
+    String::from("PenisPenis")
+}
